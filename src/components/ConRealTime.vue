08470802c45a215a34293384ad12ef1c141ee0b5
1059810269@qq.com <jiahuan19970802>
2020-08-23 23:39:54 +0800

    新加的反控功能只做了一点点。。。。。 提交漏了~~~

---
+++ src/components/ConRealTime.vue
@@ -0,0 +1,35 @@
+<template>
+
+    <a-layout :style="{ background: '#fff', padding: '0px', margin: 0, minHeight:$globalCss.curHeight- 145+'px' }">
+        <!--            <a-layout-header>Header</a-layout-header>-->
+        <a-layout-content>
+            <divisorTable></divisorTable>
+        </a-layout-content>
+        <a-layout-footer>
+            <conRealTimeList></conRealTimeList>
+        </a-layout-footer>
+    </a-layout>
+</template>
+
+<script>
+    import divisorTable from "./DivisorTable";
+    import conRealTimeList from "./ConRealTimeList";
+
+    export default {
+        data() {
+            return {
+                tabBarStyle: {
+                    // backgroundColor:'black'
+                }
+            };
+        },
+        components: {
+            divisorTable,
+            conRealTimeList
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ConRealTimeList.vue
@@ -0,0 +1,48 @@
+<template>
+    <a-list
+            class="demo-loadmore-list"
+            :loading="loading"
+            item-layout="horizontal"
+            :data-source="data"
+    >
+        <a-list-item slot="renderItem" slot-scope="item">
+            <a slot="actions">edit</a>
+            <a slot="actions">more</a>
+            <a-list-item-meta
+                    description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+            >
+                <a slot="title" href="https://www.antdv.com/">{{ item.name.last }}</a>
+                <a-avatar
+                        slot="avatar"
+                        src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+                />
+            </a-list-item-meta>
+            <div>content</div>
+        </a-list-item>
+    </a-list>
+</template>
+<script>
+    // import reqwest from 'reqwest';
+
+    // const fakeDataUrl = 'https://randomuser.me/api/?results=5&inc=name,gender,email,nat&noinfo';
+
+    export default {
+        data() {
+            return {
+                loading: true,
+                // loadingMore: false,
+                // showLoadingMore: true,
+                data: [],
+            };
+        },
+        mounted() {
+        },
+        methods: {
+        },
+    };
+</script>
+<style>
+    .demo-loadmore-list {
+        min-height: 350px;
+    }
+</style>
